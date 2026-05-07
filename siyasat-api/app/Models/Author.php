ecf0d898244b69c6a8a1cdd766015a0ef980d5e0
pebble_na_bato <feevolinto123@gmail.com>
2026-05-08 00:02:20 +0800

    Update Author.php

--- siyasat-api/app/Models/Author.php
+++ siyasat-api/app/Models/Author.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     protected $table = 'author';
+    protected $primaryKey = 'author_id';
     public $timestamps = false;
 
     protected $fillable = [
